975d1054b9b8d1862b92d31aeb19a5944cafe5b2
Jacky Nguyen <nktpro@gmail.com>
2021-12-03 08:36:29 -0500

    Update flake.lock

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Nix configuration";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/21.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     darwin = {
       url = "github:lnl7/nix-darwin";
